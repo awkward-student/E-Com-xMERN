0cd1b5eec608e3dea5f25bc3babaf76956c9f783
Vishwas <im.vishwas.03@gmail.com>
2023-07-07 15:21:06 +0530

    Create multiple basic pages in /client
    Registration, Login, Home, Contact, About, Not Found and Privacy Policy Page

---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout/Layout";
+
+const Home = () => {
+  return (
+    <Layout title={"Shopista"}>
+      <h1>Home</h1>
+    </Layout>
+  );
+};
+
+export default Home;
