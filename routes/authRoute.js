447239186dcfe3ac2b96339a3c5626a3a90e61b1
Vishwas <im.vishwas.03@gmail.com>
2023-07-07 15:24:41 +0530

    Added Forgot Password Feature and Other Route and Controllers

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,9 +2,10 @@ import express from "express";
 import {
   registerController,
   loginController,
+  forgotPasswordController,
   testController,
 } from "../controllers/authController.js";
-import { requireSignIn } from "../middlewares/authMiddleware.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 
 // router object
 const router = express.Router();
@@ -17,7 +18,20 @@ router.post("/register", registerController);
 // Login - Method: POST
 router.post("/login", loginController);
 
+// Forgot Password - Method: POST
+router.post("/forgot-password", forgotPasswordController);
+
+// Protected Route Auth - User
+router.get("/user-auth", requireSignIn, (req, res) => {
+  res.status(200).send({ ok: true });
+});
+
+// Protected Route Auth - Admin
+router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
+  res.status(200).send({ ok: true });
+});
+
 // Test Route
-router.get("/test", requireSignIn, testController);
+router.get("/test", requireSignIn, isAdmin, testController);
 
 export default router;
